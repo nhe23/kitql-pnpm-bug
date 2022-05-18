32855935e05af13a5857c3d4af64ed97a721f0ad
jycouet <jycouet@gmail.com>
2022-05-18 17:26:17 +0200

    :children_crossing: FIX: typo issue in the doc of KitQL (already fixed)

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import watchAndRun from '@kitql/vite-plugin-watch-and-run.js';
+import watchAndRun from '@kitql/vite-plugin-watch-and-run';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
